bda7a9fda23e764fe61933d07b11f26413ffb059
vmhungltt <vuminhhungltt904@gmail.com>
2022-11-28 11:39:47 +0700

    add footer

--- controllers/footer/FooterController.js
+++ controllers/footer/FooterController.js
@@ -1,14 +1,22 @@
 const Footer = require('../../models').Footer;
-
+const Post = require('../../models').Post;
 class FooterController {
   async index(req, res){
-   
+   const post_suggest = await Post.findAll({
+      offset : 3,
+      limit: 3 ,
+      order: [['id', 'DESC']],
+      attributes: ['createdAt','title', 'thumb', 'slug']
+  }); 
      const result = await Footer.findOne({
         where : {
             slug : req.body.slug
         }
      });
-     return res.json(result);
+     return res.json({
+      result,
+      post_suggest
+     });
   }
 }
 module.exports = new FooterController();
