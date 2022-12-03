bed19f4c26afa501cef73ef75a59232d1375b02e
vmhungltt <vuminhhungltt904@gmail.com>
2022-12-04 00:18:31 +0700

    ubuntu

--- config/upload/multerConfig.js
+++ config/upload/multerConfig.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'src/public/img/images/');
+      cb(null, 'src\\public\\img\\images\\');
     },
     filename: function (req, file, cb) {
       
